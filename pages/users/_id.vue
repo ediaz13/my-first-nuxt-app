7be0481fe41f6208552b9dacca063421c88dc267
ediaz13 <diazemanuel27@gmail.com>
2020-08-09 18:14:12 -0300

    16. Valiidating parameters

---
+++ pages/users/_id.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>A single user page, with Id : {{$route.params.id}} </h1>
+</template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.id); 
+    }
+}
+</script>
